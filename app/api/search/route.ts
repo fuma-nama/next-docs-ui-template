4d7c4eab581fae4829953bd7ffb25626dceb4b89
SonMooSans <god63820869@gmail.com>
2023-10-22 09:44:02 +0800

    Update to 3.0.0

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,13 +1,13 @@
-import { getPageUrl } from '@/app/source'
-import { allDocs } from 'contentlayer/generated'
-import { createSearchAPI } from 'next-docs-zeta/server'
+import { getPageUrl } from "@/app/source";
+import { allDocs } from "contentlayer/generated";
+import { createSearchAPI } from "next-docs-zeta/search/server";
 
-export const { GET } = createSearchAPI('advanced', {
-  indexes: allDocs.map(page => ({
+export const { GET } = createSearchAPI("advanced", {
+  indexes: allDocs.map((page) => ({
     id: page._id,
     title: page.title,
     content: page.body.raw,
     url: getPageUrl(page.slug),
-    structuredData: page.structuredData
-  }))
-})
+    structuredData: page.structuredData,
+  })),
+});
